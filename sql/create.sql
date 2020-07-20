c29e49653545dd3df370db93ad6debf9ef0ef1df
Pedro Dias <pdcmpa@gmail.com>
2020-07-20 01:33:39 -0300

    Corrige tipos no banco de dados

--- sql/create.sql
+++ sql/create.sql
@@ -1,11 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TYPE classificacao AS ENUM ('despesa','custo','receita');
 
 CREATE TABLE IF NOT EXISTS erp (
-	entry_id int UNIQUE NOT NULL,
-	valor int NOT NULL,
-	dia int NOT NULL,
+	id UUID DEFAULT uuid_generate_v4(),
+	valor REAL NOT NULL,
+	dia INT NOT NULL,
 	class classificacao NOT NULL,
-	origem int,
-	destino int,
-	PRIMARY KEY(entry_id)
+	origem INT,
+	destino INT,
+	PRIMARY KEY(id)
 );
--- sql/load.sql
+++ sql/load.sql
@@ -1,2 +1,9 @@
-INSERT INTO erp (entry_id, valor, dia, class, origem)
-VALUES (1, 50.5, 25, 'receita', 3)
+INSERT INTO erp (valor, dia, class, origem) VALUES 
+	(50.5, 25, 'receita', 3),
+	(49.99, 23, 'despesa', 4),
+	(20.0, 12, 'custo', 10);
+
+INSERT INTO erp (valor, dia, class, destino) VALUES 
+	(50.5, 25, 'receita', 2),
+	(123.9, 23, 'receita', 5),
+	(200.0, 12, 'despesa', 1);
